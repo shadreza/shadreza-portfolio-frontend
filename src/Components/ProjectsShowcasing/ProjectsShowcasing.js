1adaac44ac63d9e1d496b21f8fdc239724a835c2
shadreza <45334134+shadreza@users.noreply.github.com>
2021-05-02 21:41:46 +0600

    it's very beautiful when you can do a lot of stuff by one code

--- src/Components/ProjectsShowcasing/ProjectsShowcasing.js
+++ src/Components/ProjectsShowcasing/ProjectsShowcasing.js
@@ -21,13 +21,23 @@ import quran2 from '../images/quran-2.png';
 
 
 const ProjectsShowcasing = () => {
+    const infoArray=[
+        {image:[air],name:"Discover Fly",description:[],gitlinks:["https://github.com/shadreza/Discover-Fly"],livesite:"https://shadreza.github.io/Discover-Fly/"},
+        {image:[civic1,civic2,civic3,civic4],name:"Civic Shifter",description:[],gitlinks:["https://github.com/shadreza/Civic-Shifter-Website.git"],livesite:"https://civic-shifter.web.app/"},
+        {image:[slider1,slider2,slider3],name:"Fancy Slider",description:[],gitlinks:["https://github.com/shadreza/fancy-slider-resolving"],livesite:"https://shadreza.github.io/fancy-slider-resolving/"},
+        {image:[quran1,quran2],name:"Al Quran",description:[],gitlinks:["https://github.com/shadreza/Al-Quran"],livesite:"https://shadreza.github.io/Al-Quran/"},
+        {image:[shikkhok1,shikkhok2,shikkhok3,shikkhok4],name:"Shikkhok",description:[],gitlinks:["https://github.com/shadreza/Shikkhok-Website-Client.git","https://github.com/shadreza/Shikkhok-Website-Server.git"],livesite:"https://hay-store.web.app/"},
+        {image:[food1,food2],name:"Cooking Master",description:[],gitlinks:["https://github.com/shadreza/cooking-master"],livesite:"https://shadreza.github.io/cooking-master/"}
+    ]
     return (
         <div className="allProjectsDiv">
             <br/><br/>
             <h1 className="headingOfProject">Some Projects...</h1>
             <br/><br/><br/>
             <div className="gridFormat">
-                <SingleProject dataArray={[{image:[],description:[],gitlinks:[],livesite:""},{image:[],description:[],gitlinks:[],livesite:""},{image:[],description:[],gitlinks:[],livesite:""},{image:[],description:[],gitlinks:[],livesite:""},{image:[],description:[],gitlinks:[],livesite:""},{image:[],description:[],gitlinks:[],livesite:""}]}></SingleProject>
+                {
+                    infoArray.map(currentInfo=><SingleProject dataArray={currentInfo}></SingleProject>)
+                }
             </div>
         </div>
     );
--- src/Components/SingleProject/SingleProject.js
+++ src/Components/SingleProject/SingleProject.js
@@ -6,32 +6,36 @@ import Carousel from 'react-bootstrap/Carousel'
 
 const SingleProject = (passedParams) => {
     const infoArray = passedParams.dataArray;
-    
+    console.log(infoArray)
     return (
         <div>
-            <Card style={{ width: '25rem' }}>
-                <Carousel fade>
-                    {
-                        infoArray.map(image=>
-                            <Carousel.Item>
-                                <img
-                                className="d-block w-100"
-                                src={image}
-                                alt="First slide"
-                                />
-                            </Carousel.Item>
-                        )
-                    }
-                </Carousel>
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                    Some quick example text to build on the card title and make up the bulk of
-                    the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+            {
+                    <Card style={{ width: '25rem' }}>
+                        <Carousel fade>
+                            {
+                                infoArray.image.map(currentImage =>
+                                    <Carousel.Item>
+                                        <img
+                                        className="d-block w-100"
+                                        src={currentImage}
+                                        alt="First slide"
+                                        />
+                                    </Carousel.Item>
+                                )
+                            }
+                        </Carousel>
+                        <Card.Body>
+                            <Card.Title>{infoArray.name}</Card.Title>
+
+                            <Card.Text>
+                                {
+                                    infoArray.description.map(currentDescription=><p>currentDescription</p>)
+                                }
+                            </Card.Text>
+                            <Button variant="primary">Go somewhere</Button>
+                        </Card.Body>
+                    </Card>    
+            }
         </div>
     );
 };
